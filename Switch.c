55fb87e175f6e5080d82d34ce7f81add476330f6
Roman <77737080+MrWiz4rd@users.noreply.github.com>
2021-01-26 13:34:09 +0100

    Create Switch.c

---
+++ Switch.c
@@ -0,0 +1,39 @@
+/*
+*Autor: Roman Duraník
+*Copyright : 2021
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdbool.h>
+int main()
+{
+	int y;
+
+	printf("Aku znamku si dostal?: ");
+
+	scanf_s("%d", &y);
+
+	switch (y)
+	{
+	case 1:
+		printf("Si vyborny\n");
+		break;
+	case 2:
+		printf("Si chvalitebny\n");
+		break;
+	case 3:
+		printf("Si dobry\n");
+		break;
+	case 4:
+		printf("Si dostatocny\n");
+		break;
+	case 5:
+		printf("Si nedostatocny\n");
+		break;
+	default:
+		printf("Taka znamka neexistuje\n");
+		break;
+	}
+	return 0;
+}
