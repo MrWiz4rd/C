f853d1b67be9afd1bdd09cb5024eae7186745317
Roman <77737080+MrWiz4rd@users.noreply.github.com>
2021-01-26 11:40:58 +0100

    Add files via upload

---
+++ Data_types.c
@@ -0,0 +1,15 @@
+/*
+*Autor: Roman Duraník
+*Copyright : 2021
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdbool.h>
+int main()
+{
+	char a;
+		a = '+';
+	printf("Char sa rovna: %c\n\n", a);
+
+}
---
+++ Printf_Scanf.c
@@ -0,0 +1,36 @@
+/*
+*Autor: Roman Duraník
+*Copyright: 2021
+
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char str[100];
+
+    int i;
+
+
+
+    printf("Zadaj cislo: ");
+
+
+
+    scanf("%d", &i);
+
+
+
+    printf("\nZadal si: %d\n", i);
+
+
+
+    return 0;
+
+}
+
+   */ 
+
+
+
+
