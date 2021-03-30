8c2f53bd3312e96163fd0fcb84202fc514c0ece7
Roman <77737080+MrWiz4rd@users.noreply.github.com>
2021-03-30 13:04:46 +0200

    Create index.c

---
+++ Challenges/index.c
@@ -0,0 +1,48 @@
+/*
+*Autor: Roman Duraník
+*Copyright: 2021
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+int main()
+{	
+	int ID;
+	int hod;
+	float vyplata; 
+	
+
+	printf("Daj mi tvoje ID \n");
+	scanf("%d", &ID);
+
+
+	switch (ID)
+	{
+	case 1:
+		printf("User Roman\n");
+		break;
+	case 2:
+		printf("User martin\n");
+		break;
+	default: printf("Neznamy user\n");
+		break;
+	}
+
+	if (ID > 2 || ID == 0)
+	{
+		return 0;
+	}
+	else {
+		printf("Kolko hodin si odpracoval? \n");
+		scanf("%d", &hod);
+
+
+		vyplata = 1500 * hod;
+
+		printf("ID zamestnanca je %d\nZa mesiac odpracoval %d hodin\nJeho vyplata je %.2f eur\n\n\n", ID, hod, vyplata);
+
+
+
+	}
+}
