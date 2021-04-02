ba89b209f576f0f1337e578417ab5f7695d48750
Roman <77737080+MrWiz4rd@users.noreply.github.com>
2021-04-02 22:52:21 +0200

    Create personal_data.c

---
+++ Challenges/personal_data.c
@@ -0,0 +1,16 @@
+/*
+*Autor: Mr.Wizard
+*Copyright: 2021
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+int main()
+{
+	printf("Name: Roman");
+	printf("DOB: 22.5.1275");
+	printf("Mobile: 735-560-8");
+
+	return 0; 
+}
