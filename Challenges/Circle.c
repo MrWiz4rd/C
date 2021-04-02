138090c61d805cf2b34015e82f0be1ffa4c1bfe6
Roman <77737080+MrWiz4rd@users.noreply.github.com>
2021-04-02 23:03:53 +0200

    Create Circle.c

---
+++ Challenges/Circle.c
@@ -0,0 +1,22 @@
+/*
+*Autor: Mr.Wizard
+*Copyright: 2021
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+int main()
+{
+	int r = 6;
+	float pi = 3.14;
+	double obvod, obsah;
+
+	obvod = 2 * pi * r;
+	printf("Obvod kruhu je %lf", obvod);
+
+	obsah = pi * r * r;
+	printf("\nObsah kruhu je %lf", obsah);
+
+	return 0;
+}
