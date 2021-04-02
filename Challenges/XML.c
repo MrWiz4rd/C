cc5550977af0c806ed0810fe6bd7db4ff8d2a09e
Roman <77737080+MrWiz4rd@users.noreply.github.com>
2021-04-02 22:56:05 +0200

    Create XML.c

---
+++ Challenges/XML.c
@@ -0,0 +1,16 @@
+/*
+*Autor: Mr.Wizard
+*Copyright: 2021
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+int main()
+{
+	char a = 'X';
+	char b = 'M';
+	char c = 'L';
+
+	printf("%c %c %c reversed is %c %c %c", a, b, c, c, b, a);
+}
