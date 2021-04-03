cc6e850b585330891f9b7a88bae275159c2cc942
Roman <77737080+MrWiz4rd@users.noreply.github.com>
2021-04-03 17:09:34 +0200

    Create sum.c

---
+++ Challenges/sum.c
@@ -0,0 +1,25 @@
+/*
+*Autor: Mr.Wizard
+*Copyright: 2021
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+int main()
+{	
+	int a, b, vys;
+	printf("zadaj mi 2 cisla: \n");
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+	vys = a + b;
+
+	printf("sucet tychto cisiel je: %d", vys);
+
+
+
+
+
+	return 0;
+}
