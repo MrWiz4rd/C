67b757ea4d6a588e622f525dc49d2e9ebafbd144
Roman <77737080+MrWiz4rd@users.noreply.github.com>
2021-04-03 18:07:17 +0200

    Create even numbers.c

---
+++ Challenges/even numbers.c
@@ -0,0 +1,18 @@
+/*
+*Autor: Mr.Wizard
+*Copyright: 2021
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+int main()
+{	
+	for ( int i = 1; i <= 50; i++)
+	{
+		if (i % 2 == 0)
+			{
+			printf("%d ", i);
+		}
+	}
+}
