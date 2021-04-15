a9905144aa152aab4be9a4a006b8d4735dc336a1
Roman <77737080+MrWiz4rd@users.noreply.github.com>
2021-04-15 15:05:37 +0200

    Create Calculator.c

---
+++ Challenges/Calculator.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+
+int main()
+{
+	printf("Kalkulacka by Roman Duranik \n\n\n\n");
+
+	float a, b, vysledok;
+	char znak;
+
+	printf("Co chces spocitat?: ");
+	scanf("%f %c %f", &a, &znak, &b);
+	
+
+	switch (znak)
+	{
+	case '+':
+		vysledok = a + b;
+		printf("\nvysledok je %.2f", vysledok);
+		break;
+	case '-':
+		vysledok = a - b;
+		printf("\nvysledok je %.2f", vysledok);
+		break;
+	case '*':
+		vysledok = a * b;
+		printf("vysledok je %.2f", vysledok);
+		break;
+	case '/':
+		vysledok = a / b;
+		printf("\nvysledok je %.2f", vysledok);
+		break;
+	default:
+		printf("\nzadal si zly parameter");
+		break;
+	}
+	
+	return 0;
+}
