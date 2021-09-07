caea2cbdd32fc693b7aff7b1074489e8bd4d5d77
Roman <77737080+MrWiz4rd@users.noreply.github.com>
2021-09-07 17:08:28 +0200

    Update ID.c

--- Challenges/ID.c
+++ Challenges/ID.c
@@ -7,42 +7,54 @@
 #include<conio.h>
 #include<windows.h>
 int main()
-{	
+{
 	int ID;
 	int hod;
-	float vyplata; 
-	
+	float vyplata;
+	int heslo = 7355608;
+	int h;
 
-	printf("Daj mi tvoje ID \n");
-	scanf("%d", &ID);
-
-
-	switch (ID)
+	printf("Zadaj heslo: ");
+	scanf("%d", &h);
+	if (h != heslo)
 	{
-	case 1:
-		printf("User Roman\n");
-		break;
-	case 2:
-		printf("User martin\n");
-		break;
-	default: printf("Neznamy user\n");
-		break;
+		return;
 	}
-
-	if (ID > 2 || ID == 0)
+	else
 	{
-		return 0;
+		printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		printf("Zadajte vase ID: \n");
+		scanf("%d", &ID);
+		
+		switch (ID)
+		{
+		case 6218:
+			printf("Vitajte Roman!\n\n\n\n\n\n");
+			break;
+		case 15973:
+			printf("Vitajte Vlado!\n\n\n\n\n\n");
+		default:
+			printf("Neznámy user");
+			break;
+		}
+
+		if (ID == 6218 || ID == 15973)
+		{
+			printf("Kolko hodín si odpracoval?: \n");
+			scanf("%d", &hod);
+			vyplata = 5.5f * hod;
+			printf("Zarobil si %.2f €", vyplata);
+		}
+		else
+		{
+			return;
+		}
+
 	}
-	else {
-		printf("Kolko hodin si odpracoval? \n");
-		scanf("%d", &hod);
-
-
-		vyplata = 1500 * hod;
-
-		printf("ID zamestnanca je %d\nZa mesiac odpracoval %d hodin\nJeho vyplata je %.2f eur\n\n\n", ID, hod, vyplata);
 
 
 
-	}
+
+
+
 }
