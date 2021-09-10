f7cb380da3bf06fa346b7dc792a819dee2c63c99
Roman <77737080+MrWiz4rd@users.noreply.github.com>
2021-09-10 10:25:08 +0200

    FIXED ked tak este skusit s do - while

--- Challenges/ID.c
+++ Challenges/ID.c
@@ -54,7 +54,7 @@ int main()
 		{
 			printf("Kolko hodin si odpracoval?: \n");
 			scanf("%d", &hod);
-			vyplata = 5.5 * hod;
+			vyplata = 5.5f * hod;
 			printf("Zarobil si %.2f €\n\n\n\n\n", vyplata);
 		}
 
@@ -69,31 +69,31 @@ int main()
 			if (heslo == h)
 			{
 				medzera();
-		printf("Zadajte vase ID: \n");
-		scanf("%d", &ID);
+				printf("Zadajte vase ID: \n");
+				scanf("%d", &ID);
 
-		switch (ID)
-		{
-		case 6315:
-			medzera();
-			printf("Vitajte Roman!\n\n");
-			break;
-		case 8423:
-			medzera();
-			printf("Vitajte Vlado!\n\n");
-		default:
-			printf("Neznamy user");
-			return;
-		}
-
-		if (ID == 6315 || ID == 8426)
-		{
-			printf("Kolko hodin si odpracoval?: \n");
-			scanf("%d", &hod);
-			vyplata = 5.5f * hod;
-			printf("Zarobil si %.2f €\n\n\n\n\n", vyplata);
-			break;
-		}
+				switch (ID)
+			{
+			case 6315:
+				medzera();
+				printf("Vitajte Roman!\n\n");
+				break;
+			case 8423:
+				medzera();
+				printf("Vitajte Vlado!\n\n");
+			default:
+				printf("Neznamy user");
+				return;
+			}
+	
+			if (ID == 6315 || ID == 8426)
+			{
+				printf("Kolko hodin si odpracoval?: \n");
+				scanf("%d", &hod);
+				vyplata = 5.5f * hod;
+				printf("Zarobil si %.2f €\n\n\n\n\n", vyplata);
+				break;
+			}	
 			}
 		}
 
