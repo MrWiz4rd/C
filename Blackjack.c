27ca786fb55000dbfc5c8b99206a8dde7a14dff2
Roman <77737080+MrWiz4rd@users.noreply.github.com>
2021-11-05 07:44:05 +0100

    Create Blackjack.c

---
+++ Blackjack.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+
+#define ZACIATOCNEPENIAZE 10000
+
+int main()
+{
+		int PridanePeniaze, KonecnePeniaze, StavenePeniaze, Zostatok;
+	
+		
+	printf("*****************WELCOME TO THE BLACKJACK****************\n\n\n\n\n\n");
+	printf("Your starting money is %d\n\n\n\n\n", ZACIATOCNEPENIAZE);
+	printf("How much money you would like to add?: ", PridanePeniaze);
+	scanf("%d", &PridanePeniaze);
+	
+	KonecnePeniaze = ZACIATOCNEPENIAZE + PridanePeniaze;
+	
+	printf("Your money is now %d\n\n", KonecnePeniaze);
+	
+	printf("How much money do you want to bet?: ");
+	scanf("%d", &StavenePeniaze);
+		
+	Zostatok = KonecnePeniaze - StavenePeniaze;
+	printf("You bet %d and you have %d money left", StavenePeniaze, Zostatok);
+	
+		
+}
